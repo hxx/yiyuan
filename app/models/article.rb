c968b96f9a67efb57a4eb71aa53b2f4ae977e995
yellowstar <845059331@qq.com>
2014-06-09 09:58:51 +0800

    some message of validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,22 @@
 class Article < ActiveRecord::Base
   has_many :comments
   validates :title, presence: true,
-            length: { minimum: 5 } 
+            length: { 
+              minimum: 5,
+              too_short: "must have at least %{count} words" 
+            }
+  after_initialize do |article|
+    puts "You have initialize an object!"
+  end
+
+  after_find do |article|
+    puts "You have found an object!"
+  end
+
+  after_touch :after_touch_message
+
+  private
+    def after_touch_message
+      puts "You have touched an object again!"
+    end
 end
