722a27c7319321a265aea165b8bc5f55c7dc0ec8
yellowstar <845059331@qq.com>
2014-06-11 21:49:34 +0800

    associations among models

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,2 +1,6 @@
 class Hospital < ActiveRecord::Base
+  has_and_belongs_to_many :areas
+  has_many                :departments
+  has_many                :products
+  belongs_to              :city
 end
