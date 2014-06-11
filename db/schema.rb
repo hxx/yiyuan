3fcc8268b400fdc87045af6fa27c10f09543c51a
yellowstar <845059331@qq.com>
2014-06-11 21:34:26 +0800

    create model City

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611133226) do
+ActiveRecord::Schema.define(version: 20140611133336) do
+
+  create_table "cities", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "clients", force: true do |t|
     t.string   "name"
