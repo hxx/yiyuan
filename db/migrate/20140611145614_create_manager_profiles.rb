d35285a7e70824018f1d06707b02badf7aa1b0ca
yellowstar <845059331@qq.com>
2014-06-11 22:57:44 +0800

    create model ManagerProfile

---
+++ db/migrate/20140611145614_create_manager_profiles.rb
@@ -0,0 +1,24 @@
+class CreateManagerProfiles < ActiveRecord::Migration
+  def change
+    create_table :manager_profiles do |t|
+      t.integer :manager_id
+      t.string  :staff_code
+      t.string  :name
+      t.string  :gender
+      t.string  :id_card
+      t.date    :birthday
+      t.integer :age
+      t.integer :weight
+      t.integer :height
+      t.string  :native_place
+      t.string  :race
+      t.string  :political_status
+      t.string  :education
+      t.string  :address
+      t.string  :phone
+      t.string  :photo
+
+      t.timestamps
+    end
+  end
+end
