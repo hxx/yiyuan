7f24b7f54b280c03fe908e7660d1734212a6ed2a
yellowstar <845059331@qq.com>
2014-06-14 13:38:16 +0800

    add new foreign key to orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613150033) do
+ActiveRecord::Schema.define(version: 20140614053641) do
 
   create_table "areas", force: true do |t|
     t.string   "name"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20140613150033) do
     t.integer  "client_id"
     t.integer  "product_id"
     t.integer  "creator_id"
+    t.integer  "current_service_log_id"
     t.string   "state"
     t.datetime "service_start_time"
     t.datetime "service_end_time"
