27c080d085a37cb3bcbd72c8756db7becf887078
yellowstar <845059331@qq.com>
2014-06-21 14:06:14 +0800

    association between manager/operator and orders

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -2,4 +2,6 @@ class Manager < ActiveRecord::Base
   has_one :credential, as: :credentialable
 
   has_and_belongs_to_many :hospitals
+
+  has_many :orders, as: :orderable
 end
--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,4 +1,6 @@
 class Operator < ActiveRecord::Base
   has_one :credential, as: :credentialable
   belongs_to :hospital
+
+  has_many :orders, as: :orderable
 end
