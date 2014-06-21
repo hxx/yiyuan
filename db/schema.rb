479c68198e87f2a2ae85f3910589f86eb8774a75
yellowstar <845059331@qq.com>
2014-06-21 16:11:06 +0800

    create table ability_tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621062357) do
+ActiveRecord::Schema.define(version: 20140621081017) do
+
+  create_table "ability_tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "administrators", force: true do |t|
     t.datetime "created_at"
