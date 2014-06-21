528c00a3b7b94ccd00ce8b38a0c4d04f32d5130e
yellowstar <845059331@qq.com>
2014-06-21 13:34:44 +0800

    add credentialable polymorphic foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621052849) do
+ActiveRecord::Schema.define(version: 20140621053327) do
 
   create_table "areas", force: true do |t|
     t.string   "name"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20140621052849) do
 
   create_table "credentials", force: true do |t|
     t.string   "email"
+    t.integer  "credentialable_id"
+    t.string   "credentialable_type"
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
