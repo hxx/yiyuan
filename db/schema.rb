c68d658ea6caaa67a3dd40d95b83be5d51263a14
yellowstar <845059331@qq.com>
2014-06-21 13:54:07 +0800

    create new table administrators

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621055141) do
+ActiveRecord::Schema.define(version: 20140621055318) do
+
+  create_table "administrators", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "areas", force: true do |t|
     t.string   "name"
