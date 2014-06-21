c24242b9e0eabf7135a9f554a391e0e832729675
yellowstar <845059331@qq.com>
2014-06-21 16:17:05 +0800

    setup association between carers and ability_tags

--- app/models/ability_tag.rb
+++ app/models/ability_tag.rb
@@ -1,3 +1,5 @@
 class AbilityTag < ActiveRecord::Base
   has_and_belongs_to_many :products
+
+  has_and_belongs_to_many :carers, join_table: 'ability_tags_carers'
 end
--- app/models/carer.rb
+++ app/models/carer.rb
@@ -2,4 +2,6 @@ class Carer < ActiveRecord::Base
   belongs_to :hospital
 
   has_many :orders, as: :creator
+
+  has_and_belongs_to_many :ability_tags, join_table: 'ability_tags_carers'
 end
