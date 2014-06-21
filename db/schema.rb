c8c701d77aad944af5a622c997d0ef755d0648d3
yellowstar <845059331@qq.com>
2014-06-21 13:38:00 +0800

    remove type column from credentials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621053327) do
+ActiveRecord::Schema.define(version: 20140621053520) do
 
   create_table "areas", force: true do |t|
     t.string   "name"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20140621053327) do
     t.string   "email"
     t.integer  "credentialable_id"
     t.string   "credentialable_type"
-    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
