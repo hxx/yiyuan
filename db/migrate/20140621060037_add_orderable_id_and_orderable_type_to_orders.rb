75806bc4f9ee84ad9a7727d454931e8a11d36380
yellowstar <845059331@qq.com>
2014-06-21 14:09:11 +0800

    rollback migration

--- db/migrate/20140621060037_add_orderable_id_and_orderable_type_to_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddOrderableIdAndOrderableTypeToOrders < ActiveRecord::Migration
-  def change
-    add_column :orders, :orderable_id, :integer, after: :product_id
-    add_column :orders, :orderable_type, :string, after: :orderable_id
-  end
-end
