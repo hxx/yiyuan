e92ab86f776511adfb81b1c58a65636b0fe9ede2
yellowstar <845059331@qq.com>
2014-06-22 21:59:32 +0800

    rails generate devise Credential

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -1,6 +1,8 @@
 class Credential < ActiveRecord::Base
   belongs_to :credentialable, polymorphic: true
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
